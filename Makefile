65958f373b0c3ea8efa6bcdffe582693e8456d98
Wei Jie <tayweijie777@gmail.com>
2023-03-15 12:32:45 -0400

    added init in makefile

--- Makefile
+++ Makefile
@@ -7,3 +7,6 @@ client:
 
 server:
 	python3 server.py
+
+init:
+	python3 -m grpc_tools.protoc -I=. proto/*.proto --python_out=. --grpc_python_out=.
--- server.py
+++ server.py
@@ -14,15 +14,15 @@ SIGNAL_HOST = '0.0.0.0:50052'
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     
-    try: 
-        opaque_pb2_grpc.add_OpaqueAuthenticationServicer_to_server(OpaqueAuthenticationServicer(), server)
-        server.add_insecure_port(OPAQUE_HOST)
+    opaque_pb2_grpc.add_OpaqueAuthenticationServicer_to_server(OpaqueAuthenticationServicer(), server)
+    server.add_insecure_port(OPAQUE_HOST)
 
-        signalc_pb2_grpc.add_SignalKeyDistributionServicer_to_server(SignalKeyDistribution(), server)
-        server.add_insecure_port(SIGNAL_HOST)
-        
-        logger.info("Server has started")
-        server.start()
+    signalc_pb2_grpc.add_SignalKeyDistributionServicer_to_server(SignalKeyDistribution(), server)
+    server.add_insecure_port(SIGNAL_HOST)
+    
+    logger.info("Server has started")
+    server.start()
+    try:
         server.wait_for_termination()
     except KeyboardInterrupt:
         server.stop(5)
