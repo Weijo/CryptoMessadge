ac2c578220ec76d2011405fe774dc2a94a873444
ng-wl <weiliangng23@yahoo.com>
2023-03-24 13:28:11 -0400

    changed the way SignalClient is called in test.py

--- test.py
+++ test.py
@@ -33,8 +33,9 @@ def main():
             else:
                 logger.warning("Invalid type choose 1 or 2")
 
+
         # Chatting
-        with SignalClient(username, 12345, HOST, SIGNAL_PORT, CERTIFILE_FILE, token) as signalClient:
+        with SignalClient(12345, HOST, SIGNAL_PORT, CERTIFILE_FILE, username, token) as signalClient:
             signalClient.subscribe()
             signalClient.register_keys(1, 1)
 
